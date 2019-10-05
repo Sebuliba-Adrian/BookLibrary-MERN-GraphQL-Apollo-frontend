33187a295665d0b5d815df46f8afb7dba757d8fb
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-05 11:36:50 +0300

    Add book creation logic

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { graphql } from "react-apollo";
-import { getAuthorsQuery } from "../queries/queries";
+import { graphql} from "react-apollo";
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from "../queries/queries";
+import * as compose from 'lodash.flowright';
+
 
 class AddBook extends Component {
   constructor(props) {
@@ -12,7 +14,8 @@ class AddBook extends Component {
     };
   }
   displayAuthors() {
-    var data = this.props.data;
+    var data = this.props.getAuthorsQuery;
+    console.log(this.props); //appears like loadin has changed
     if (data.loading) {
       return <option>Loading Authors...</option>;
     } else {
@@ -27,6 +30,14 @@ class AddBook extends Component {
   submitForm(e){
       e.preventDefault();
       console.log(this.state);
+     this.props.addBookMutation({
+       variables: {
+         name: this.state.name,
+         genre: this.state.genre,
+         authorId: this.state.authorId
+       },
+       refetchQueries: [{query: getBooksQuery}]
+     })
 
   }
   render() {
@@ -61,4 +72,7 @@ class AddBook extends Component {
     );
   }
 }
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, {name: "getAuthorsQuery"}),
+    graphql(addBookMutation, { name: "addBookMutation"})
+)(AddBook);
