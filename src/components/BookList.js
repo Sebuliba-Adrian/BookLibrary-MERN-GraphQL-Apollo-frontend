99df44d6647f5f975efb1d9e5819c28430df0164
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-06 01:34:11 +0300

    add ability to view book details

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  state = {
+    selected: null
+  };
   displayBooks() {
     var data = this.props.data;
     if (data.loading) {
       return <div>Loading books...</div>;
     } else {
-      return data.books.map(book => <li key={book.id}>{book.name}</li>);
+      return data.books.map(book => (
+        <li
+          key={book.id}
+          onClick={e => {
+            this.setState({ selected: book.id });
+          }}
+        >
+          {book.name}
+        </li>
+      ));
     }
   }
   render() {
@@ -16,6 +29,7 @@ class BookList extends Component {
     return (
       <div>
         <ul>{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
